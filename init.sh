239b3b0e2e4f3d57c7cf2672399c124c69ad1964
dev-clavis <declavis.company@gmail.com>
2021-09-09 23:54:01 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+wget https://raw.githubusercontent.com/dev-clavis/util-s/main/img.sh
+wget https://raw.githubusercontent.com/dev-clavis/util-s/main/pb.sh
+wget https://raw.githubusercontent.com/dev-clavis/util-s/main/mark1.sh
+wget https://raw.githubusercontent.com/dev-clavis/util-s/main/mark2.sh
+wget https://raw.githubusercontent.com/dev-clavis/util-s/main/sc.sh
+
+chmod +x img.sh
+chmod +x pb.sh
+chmod +x mark1.sh
+chmod +x mark2.sh
+chmod +x sc.sh
